9e0c6742bc238468150f6b633c9590aae1414bde
John A Seubert <jaseubert@Johns-MacBook-Pro-2.local>
2014-08-22 19:21:56 -0700

    Initial Commit

---
+++ Thatso/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Thatso
+//
+//  Created by John A Seubert on 8/22/14.
+//  Copyright (c) 2014 John Seubert. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Thatso/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Thatso
+//
+//  Created by John A Seubert on 8/22/14.
+//  Copyright (c) 2014 John Seubert. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
